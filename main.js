798fc98bfae0c3cc730e0cb17caedc4aaa2622de
deriknyvo <derikoliveiranyhc@hotmail.com>
2022-04-18 21:27:03 -0300

    testando requestAnimationFrame

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const canvas = document.getElementById('my-canvas');
 const context = canvas.getContext('2d');
 
-const image = new Image();
-image.src = 'img/explosao.png';
+let x = 20;
+const y = 100;
+const radius = 5;
+const startAngle = 0;
+const endAngle = Math.PI * 2;
 
-image.onload = () => {
-    context.drawImage(
-        image,
-        80, 10, 60, 65,
-        20, 20, 60, 65
-    );
-};
+requestAnimationFrame(mexerBola);
+
+function mexerBola() {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.height);
+    context.beginPath();
+    context.arc(x, y, radius, startAngle, endAngle);
+    context.fill();
+
+    const speed = 20;
+    x += speed;
+
+    requestAnimationFrame(mexerBola);
+}

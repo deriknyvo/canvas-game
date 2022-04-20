95f0df025840223353c6cbaa84d28fae9505b3bb
deriknyvo <derikoliveiranyhc@hotmail.com>
2022-04-19 22:59:35 -0300

    feat(balls): adicionando novas bolas ao canvas

--- main.js
+++ main.js
@@ -1,42 +1,32 @@
 const canvas = document.getElementById('my-canvas');
 const context = canvas.getContext('2d');
 
-let x = 20;
-let y = 100;
-const radius = 5;
-const startAngle = 0;
-const endAngle = Math.PI * 2;
-const speed = 5;
+const b1 = new Bola(context);
+b1.x = 100;
+b1.y = 200;
+b1.velocidadeX = 20;
+b1.velocidadeY = -10;
+b1.cor = 'red';
+b1.raio = 20;
 
-requestAnimationFrame(mexerBola);
+const b2 = new Bola(context);
+b2.x = 200;
+b2.y = 100;
+b2.velocidadeX = -10;
+b2.velocidadeY = 20;
+b2.cor = 'blue';
+b2.raio = 30;
 
-function mexerBola() {
-    context.clearRect(0, 0, canvas.clientWidth, canvas.height);
-    context.beginPath();
-    context.arc(x, y, radius, startAngle, endAngle);
-    context.fill();
+const b3 = new Bola(context);
+b3.x = 159;
+b3.y = 135;
+b3.velocidadeX = -10;
+b3.velocidadeY = 20;
+b3.cor = 'green';
+b3.raio = 15;
 
-    requestAnimationFrame(mexerBola);
-}
-
-document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'ArrowUp':
-            y -= speed;
-            break;
-        
-        case 'ArrowDown':
-            y += speed;
-            break;
-    
-        case 'ArrowLeft':
-            x -= speed;
-            break;
-
-        case 'ArrowRight':
-            x += speed;
-            break;
-        default:
-            break;
-    }
-}, true);
+const animacao = new Animacao(context);
+animacao.novoSprite(b1);
+animacao.novoSprite(b2);
+animacao.novoSprite(b3);
+animacao.ligar();
